1eff4d13fd06ee488b05e8169fb4d7c4598ae64b
Ian França <93686060+IanFranco7@users.noreply.github.com>
2023-01-07 20:19:49 -0300

    Adicionando novas funcionalidades

---
+++ order.js
@@ -0,0 +1,33 @@
+const Orders = [{
+    productName: 'Foldable Mini Drone',
+    productNumber: '85631',
+    paymentStatus: 'Due',
+    shipping: 'Pending'
+},
+{
+    productName: 'LARVENDER KF02 Drone',
+    productNumber: '10421',
+    paymentStatus: 'Refunded',
+    shipping: 'Declined'
+},
+{
+    productName: 'RUKO FK 11 PRO',
+    productNumber: '77085',
+    paymentStatus: 'Due',
+    shipping: 'Pending'
+},
+{
+    productName: 'Drone with Camera Drone',
+    productNumber: '01479',
+    paymentStatus: 'Paid',
+    shipping: 'Delivered'
+},
+{
+    productName: 'GPS DRONE 4K',
+    productNumber: '89123',
+    paymentStatus: 'Paid',
+    shipping: 'Delivered'
+},
+]
+
+//orders.push

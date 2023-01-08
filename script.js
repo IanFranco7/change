c08a4d4eb3f143cf845018771c31c5f21b799094
Ian França <93686060+IanFranco7@users.noreply.github.com>
2023-01-08 15:21:41 -0300

    Add style new order

--- script.js
+++ script.js
@@ -2,6 +2,23 @@ const sideMenu = document.querySelector("aside")
 const menuBtn = document.querySelector('#menu-btn')
 const closeBtn = document.querySelector('#close-btn')
 const themeToggler = document.querySelector('.theme-toggler')
+const orderBtn = document.querySelector('.add-order')
+const closeOrder = document.querySelector('#close-order')
+
+// Abrindo uma nova ordem
+orderBtn.addEventListener('click', () =>{
+    document.querySelector('.container').style.filter = 'opacity(0.2)';
+    document.querySelector('.modal').style.display = 'flex';
+})
+
+//Fechando a ordem
+closeOrder.addEventListener('click', () =>{
+    document.querySelector('.container').style.filter ='none';
+    document.querySelector('.modal').style.display = 'none';
+})
+
+
+
 
 menuBtn.addEventListener('click', () =>{
     sideMenu.style.display = 'block';
@@ -18,7 +35,6 @@ themeToggler.addEventListener('click', () => {
     themeToggler.querySelector('span:nth-child(1)').classList.toggle('active');
     themeToggler.querySelector('span:nth-child(2)').classList.toggle('active')
   
-
 })
 
 
